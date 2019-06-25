4c15cfc42da2a7ffb8039299f5b660d67cf34076
Jacob Batterman <ctrjcb@gmail.com>
2019-06-25 15:56:44 -0600

    access contron allow origin

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ app.use (
   })
 );
 
+app.use (function (req, res, next) {
+  res.header ('Access-Control-Allow-Origin', '*');
+  res.header (
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next ();
+});
+
 app.listen (port, () => {
   console.log ("We're live on port 4444");
 });
